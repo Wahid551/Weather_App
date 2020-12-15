f1d81a3e11c41ab1dc953ac8881a6d4cda54496d
Wahid Malik <wahidmalik551@gmail.com>
2020-12-15 18:05:54 +0500

    Initial commit

---
+++ scratch_file.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void MyApp() {
+  Alltasks();
+}
+
+void Alltasks() {
+  task1();
+  task2();
+  task3();
+}
+
+void task1() {
+  String result = 'Task 1 data';
+  print('Task 1 completed');
+}
+
+void task2() {
+  String result = 'Task 2 data';
+  print('Task 2 completed');
+}
+
+void task3() {
+  String result = 'Task 3 data';
+  print('Task 3 completed');
+}
